d11c92008d03b0dcfb029c0d41523e71c0e3f37f
Hebert Oliveira <hebertoliveira@infojr.com.br>
2019-08-30 07:24:20 -0300

    Cria configurações básicas para o uso da API

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 
 import Header from './components/Header/index';
 import SearchBox from './components/SearchBox/index';
+import api from './api';
 
 class App extends Component{
 
   state = {
-    
+    results: [],
+    page: 0,
+    total_pages: 0
   }
+
+  apiKey = process.env.API_KEY
+
+  getMovies = (query) => {
+    api.get(`/search/movie?api_key=${this.apiKey}&query=${query}`)
+       .then( res => {
+         const { results, page, total_pages } = res.data;
+         this.setState({results, page, total_pages})
+      })
+  }
+
+  componentDidMount = () => this.getMovies("bad")
+
   render() {
+    
+    // console.log(this.state)
     return (
       <>
         <Header />
